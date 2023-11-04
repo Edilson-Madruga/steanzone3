4a30285fa032f90f91f4c70161fc3ac23406cbcd
edilson madruga <edilson.madruga@gmail.com>
2023-11-03 23:56:06 -0300

    foi55

--- links_html/postitteve.js
+++ links_html/postitteve.js
@@ -6,8 +6,22 @@ const imagem2 = document.getElementById("imagem2");
 const video2 = document.getElementById("video2");
 const imagem3 = document.getElementById("imagem3");
 const video3 = document.getElementById("video3");
+const imagem4 = document.getElementById("imagem4");
+const video4 = document.getElementById("video4");
 const imagem5 = document.getElementById("imagem5");
 const video5 = document.getElementById("video5");
+const imagem6 = document.getElementById("imagem6");
+const video6 = document.getElementById("video6");
+const imagem7 = document.getElementById("imagem7");
+const video7 = document.getElementById("video7");
+const imagem8 = document.getElementById("imagem8");
+const video8 = document.getElementById("video8");
+const imagem9 = document.getElementById("imagem9");
+const video9 = document.getElementById("video9");
+const imagem10 = document.getElementById("imagem10");
+const video10 = document.getElementById("video10");
+const imagem11 = document.getElementById("imagem11");
+const video11 = document.getElementById("video11");
 
 imagem0.addEventListener("mouseover", () => {
     video0.style.display = "block";
@@ -45,6 +59,15 @@ imagem3.addEventListener("mouseout", () => {
     video3.style.display = "none";
     video3.pause();
 });
+imagem4.addEventListener("mouseover", () => {
+    video4.style.display = "block";
+    video4.play();
+});
+
+imagem4.addEventListener("mouseout", () => {
+    video4.style.display = "none";
+    video4.pause();
+});
 imagem5.addEventListener("mouseover", () => {
     video5.style.display = "block";
     video5.play();
@@ -53,4 +76,58 @@ imagem5.addEventListener("mouseover", () => {
 imagem5.addEventListener("mouseout", () => {
     video5.style.display = "none";
     video5.pause();
+});
+imagem6.addEventListener("mouseover", () => {
+    video6.style.display = "block";
+    video6.play();
+});
+
+imagem6.addEventListener("mouseout", () => {
+    video6.style.display = "none";
+    video6.pause();
+});
+imagem7.addEventListener("mouseover", () => {
+    video7.style.display = "block";
+    video7.play();
+});
+
+imagem7.addEventListener("mouseout", () => {
+    video7.style.display = "none";
+    video7.pause();
+});
+imagem8.addEventListener("mouseover", () => {
+    video8.style.display = "block";
+    video8.play();
+});
+
+imagem8.addEventListener("mouseout", () => {
+    video8.style.display = "none";
+    video8.pause();
+});
+imagem9.addEventListener("mouseover", () => {
+    video9.style.display = "block";
+    video9.play();
+});
+
+imagem9.addEventListener("mouseout", () => {
+    video9.style.display = "none";
+    video9.pause();
+});
+imagem10.addEventListener("mouseover", () => {
+    video10.style.display = "block";
+    video10.play();
+});
+
+imagem10.addEventListener("mouseout", () => {
+    video10.style.display = "none";
+    video10.pause();
+});
+imagem11.addEventListener("mouseover", () => {
+    video11.style.display = "block";
+    video11.play();
+});
+
+imagem11.addEventListener("mouseout", () => {
+    video11.style.display = "none";
+    video11.pause();
 });
